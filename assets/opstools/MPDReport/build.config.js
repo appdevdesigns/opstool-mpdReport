b7895292f1c89388b5f7c1a6f9fe2a744338d9ac
Johnny Hausman <johnny@appdevdesigns.net>
2015-02-17 02:10:27 +0700

    + Add: included dropzone.js library into OpsPortal

--- assets/opstools/MPDReport/build.config.js
+++ assets/opstools/MPDReport/build.config.js
@@ -1,13 +1,13 @@
 module.exports={
     // map: {
     // },
-    paths: {
-        "dropzone.js" : "js/dropzone.min.js",
-        "dropzone.css" : "styles/dropzone.css"
-    },
+    // paths: {
+    //     // "dropzone.js" : "js/dropzone.min.js",
+    //     // "dropzone.css" : "styles/dropzone.css"
+    // },
     shim : {
-        'dropzone.js' : { packaged:false },
-        'dropzone.css' : {packaged:false },
+        // 'dropzone.js' : { packaged:false },
+        // 'dropzone.css' : {packaged:false },
         'site/labels/opstool-MPDReport.js' : { packaged:false, ignore:true }
     }
     // ext: {
